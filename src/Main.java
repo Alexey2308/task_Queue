a4d2b8cc94530fe2dec18ee035e58f140ce38d4b
Alexey Elagin <alexyel@mail.ru>
2022-08-15 23:52:39 +0300

    Main. and Person. added

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+
+public class Main {
+
+    public static List<Person> generateClients() {
+        List<Person> atraction = new ArrayList<>();
+        atraction.add(new Person("Владимир", "Смирнов", 10));
+        atraction.add(new Person("Иосиф", "Сталин", 20));
+        atraction.add(new Person("Владимир", "Ленин", 15));
+        atraction.add(new Person("Максим", "Перепелица", 11));
+        atraction.add(new Person("Геннадий", "Крокодил", 14));
+        atraction.add(new Person("Лаврентий", "Берия", 17));
+        atraction.add(new Person("Мимо", "Проходил", 19));
+        return atraction;
+    }
+
+    public static void main(String[] args) {
+        Queue<Object> queue = new LinkedList<>();
+        for (int i = 0; i < generateClients().size(); i++) {
+            queue.offer(generateClients().get(i));
+        }
+        while (!queue.isEmpty()) {
+            Person client = (Person) queue.poll();
+            System.out.println(client.getName() + " " + client.getSurname() + " " + "прокатился");
+            if (client.minusTicket()) {
+                queue.add(client);
+            }
+        }
+    }
+}
+
---
+++ src/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    private String name;
+    private String surname;
+    private int numberOfTickets;
+
+    public <T> Person(String name, String surname, int numberOfTickets) {
+        this.name = name;
+        this.surname = surname;
+        this.numberOfTickets = numberOfTickets;
+    }
+
+    public boolean minusTicket() {
+        if ((numberOfTickets--) <= 0) {
+            return false;
+        }
+        return true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+}
